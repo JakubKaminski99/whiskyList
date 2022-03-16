eb4b3961f877ddd9ce109c0aef63c8e80cc9b7fd
Jakub Kaminski <jakub.kaminski99@wp.pl>
2022-03-16 20:45:07 +0100

    whisky

--- whisky.py
+++ whisky.py
@@ -4,16 +4,15 @@ url = 'https://sklep-domwhisky.pl/pol_m_Scotch-Whisky_Single-malt-whisky-438.htm
 session = HTMLSession()
 res = session.get(url)
 res.html.render()
-products = res.html.xpath('//*[@id="search"]', first = True)
 lis=list()
+products = res.html.xpath('//*[@id="search"]', first = True)
 for item in products.absolute_links:
     res = session.get(item)
     if "product-pol" in str(res.html):
         name = res.html.find('div.product_info', first = True).text
         price = res.html.find('div.product_section')[1].text
         lis.append(f"{name} with the price of {price}")
-        print(price)
-        
+        print(price)      
 with open("whisky.html", "w") as file:
     for item in lis:
         file.write(f"{item} \n")
